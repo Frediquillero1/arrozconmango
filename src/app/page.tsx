af6c8f8759714ea834d0c523f36dff9657c020d1
Frediquillero1 <fredemilio@gmail.com>
2026-06-24 08:13:14 +0200

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+	return (
+		<div>
+			<h1 className='font-heading'>Este es Arroz con Mango</h1>
+			<p className='font-body'>
+				Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur qui
+				itaque iure officiis, consectetur provident natus minima, officia cum
+				consequatur ipsa rerum quasi perspiciatis alias quas esse voluptates
+				labore maiores.
+			</p>
+		</div>
+	)
+}
